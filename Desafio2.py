e7cf655d0ba1c5aa67337334c00fea0462f49323
cavalheiroeduarda-pixel <cavalheiro.eduarda@escola.pr.gov.br>
2026-03-25 15:46:32 -0300

    Update Desafio2.py

--- Desafio2.py
+++ Desafio2.py
@@ -1,9 +1,11 @@
-#
-numero = int(input("Digite um número: "))
-#
+# Solicita ao usuário que insira um número
+numero = input("Digite um número: ")
+# Converte a entrada do usuário de string para inteiro
+numero = int(numero)
+# Verifica se o número é par
 if numero % 2 == 0:
-  #
-  print(f"{numero} é par")
+  # Se o número for par, imprime uma mensagem correspondente
+    print(numero, "é um número par.")
 else:
-  #  
-  print(f"{numero} é ímpar")
+  # Se o número não for par, imprime uma mensagem correspondente
+    print(numero, "não é um número par.")
