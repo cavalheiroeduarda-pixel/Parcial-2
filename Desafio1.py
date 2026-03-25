5bf3a5da4377cf49247d8c75f2a93e689ea09b17
cavalheiroeduarda-pixel <cavalheiro.eduarda@escola.pr.gov.br>
2026-03-25 08:31:27 -0300

    Create Desafio1.py

---
+++ Desafio1.py
@@ -0,0 +1,10 @@
+#Mostra uma mensagem pedindo ao usuário para digitar dois números.
+print('Informe dois números')
+#Converte para valores numéricos
+n1 = float(input('Primeiro número: '))
+#Converte para valores numéricos
+n2 = float(input('Segundo número: '))
+#Soma esses números
+r = n1 + n2
+#Mostra o resultado da soma de dois números na tela
+print(n1, '+', n2, '=', r),
