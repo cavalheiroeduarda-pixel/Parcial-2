105cdd096ad4fb60d00f08c85dde302222009c98
cavalheiroeduarda-pixel <cavalheiro.eduarda@escola.pr.gov.br>
2026-03-31 15:24:35 -0300

    Update Desafio5.py

--- Desafio5.py
+++ Desafio5.py
@@ -1,8 +1,8 @@
 # Mostra uma mensagem inicial para o usuário
 print("Cálculo da área de um triângulo")
-# Pede ao usuário o valor da base e converte para número decimal
+# Pede ao usuário o um valor da base e converte para decimal
 base = float(input("Digite o valor da base: "))
-# Pede ao usuário o valor da altura e converte para número decimal
+# Pede ao usuário o valor da altura e converte para decimal
 altura = float(input("Digite o valor da altura: "))
 # Calcula a área do triângulo usando a fórmula (base * altura) / 2
 area = (base * altura) / 2
